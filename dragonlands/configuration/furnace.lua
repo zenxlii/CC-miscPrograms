669899a3217464d965eba18d74a7d3c178517b27
zenxlii <chloej4b6@gmail.com>
2025-06-26 15:04:49 +0100

    Update furnace.lua
    Even more recipes.

--- dragonlands/configuration/furnace.lua
+++ dragonlands/configuration/furnace.lua
@@ -21,6 +21,9 @@ addRecipe({n.stone,1},64,{{n.cobble,1}})
 addRecipe({"minecraft:smooth_stone",1},64,{{n.stone,1}})
 addRecipe({n.refIron,1},64,{{n.iron,1}})
 addRecipe({n.aAlloy,1},64,{{"techreborn:mixed_metal_ingot",1}})
+addRecipe({n.glass,1},64,{{n.sand,1}})
+addRecipe({n.dKelp,1},64,{{"minecraft:kelp",1}})
+addRecipe({"minecraft:nether_brick",1},64,{{n.nRack,1}})
 
 --Final Return Statement
 
