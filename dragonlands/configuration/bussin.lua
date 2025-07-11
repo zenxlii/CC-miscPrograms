3872ef2301a92b1e6b76c2b2b471445d0b463203
zenxlii <chloej4b6@gmail.com>
2025-07-11 14:53:21 +0100

    Update bussin.lua

--- dragonlands/configuration/bussin.lua
+++ dragonlands/configuration/bussin.lua
@@ -42,7 +42,7 @@ table.insert(taskTable, machBlock3Out)
 
 local machBlock4Out = {}
 machBlock4Out["taskType"] = "import"
-machBlock4Out["target"] = "minecraft:barrel_70"
+machBlock4Out["target"] = "minecraft:barrel_71"
 machBlock4Out["specificSlots"] = false
 table.insert(taskTable, machBlock4Out)
 
