a82139c6b188cc6919cf4ec4738ac5b12ca4191f
zenxlii <chloej4b6@gmail.com>
2025-07-25 16:21:40 +0100

    Create infiniteRetrieve.lua

---
+++ dragonlands/configuration/infiniteRetrieve.lua
@@ -0,0 +1,20 @@
+--Required Stuff
+local core = require("recipeCoreDefs")
+
+local n = core.sh
+
+local recipeList = {}
+--Relevant Recipe Function Wrapper
+local function addRecipe(productEName, batchLim, pullInv, pullSlot)
+	local eName, recipeData = core.makeInfiniteRetrieveRecipe(productEName, batchLim, pullInv, pullSlot)
+	recipeList[eName] = recipeData
+end
+--User-Defined Recipes
+
+--Below is an example recipe.
+addRecipe(n.cobble. 64, "techreborn:storage_unit_68",2)
+--Final Return Statement
+
+return{
+recipeList = recipeList
+}
